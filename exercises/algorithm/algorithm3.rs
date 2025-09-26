36b70b4a30b16928ef6f6337da7f70666fa99b89
Your Name <you@example.com>
2025-09-26 15:55:49 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -6,7 +6,30 @@
 // I AM NOT DONE
 
 fn sort<T>(array: &mut [T]){
-	//TODO
+    let n = array.len();
+    if n <= 1 {
+        return; // 空数组或只有一个元素的数组已经是排序好的
+    }
+
+    // 冒泡排序的核心逻辑
+    for i in 0..n {
+        // 标记本轮是否发生了交换，如果没有交换，说明数组已经有序
+        let mut swapped = false;
+        
+        // 每轮排序都会将最大的元素"浮"到末尾，所以每轮可以少比较一个元素
+        for j in 0..n - i - 1 {
+            if array[j] > array[j + 1] {
+                // 交换元素
+                array.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+        
+        // 如果没有发生交换，说明数组已经排序完成，可以提前退出
+        if !swapped {
+            break;
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
